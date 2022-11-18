546b269846d69963e7250a64aa0d6a17f19c7ef8
nguyenphuchaudev <nguyenphuchau.dev@gmail.com>
2022-11-18 10:33:18 +0700

    Create Admin

--- eIMIC223925.BackendAPI/Controllers/ProductsController.cs
+++ eIMIC223925.BackendAPI/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using eIMIC223925.Application.Catalog.Products;
 using eIMIC223925.ViewModels.Catalog.ProductImages;
 using eIMIC223925.ViewModels.Catalog.Products;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,6 +13,7 @@ namespace eIMIC223925.BackendAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductsController : ControllerBase
     {
         private readonly IProductService _productService;
